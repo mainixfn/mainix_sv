8b4f445172d0bf86d8afff811f93b30ffa159055
doyeong desktop <rlarltjq123@naver.com>
2020-03-31 23:13:37 +0900

    cpu 사용률을 줄이기위한 import 함수 수정

--- mainix/sise_future/Import_for_Database.py
+++ mainix/sise_future/Import_for_Database.py
@@ -26,6 +26,178 @@ class stock_future():
 
 
     # 네이버에서 실시간  시세크롤링 / 실시간 반영 / 신호속도 느림
+    def stock_naver_sise(self,stock_name):
+        list = []
+        code = self.stock_code(stock_name)
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.90.18 Safari/537.36'
+        headers = {'User-Agent': user_agent}
+        url = "http://finance.naver.com/item/sise.nhn?code=" + code
+        source = requests.get(url, headers=headers).text  # requests 모듈을 통해 텍스트로 끌어옴
+        soup = BeautifulSoup(source, 'html.parser')
+        no_today = soup.find("p", {"class": "no_today"})
+        no_exday = soup.find("p", {"class": "no_exday"})
+        sise = no_today.find("span", {"class": "blind"}).text
+        price_table = soup.find("table")
+        start_price = price_table.find_all("td", {"class": "first"})[1]
+        start_price = start_price.find("span", {"class": "blind"}).text
+        try:
+            dungrak = no_exday.find_all("em", {"class": "no_up"})[1]
+            buho = dungrak.text[1]
+
+        except:
+            try:
+                dungrak = no_exday.find_all("em", {"class": "no_down"})[1]
+                buho = dungrak.text[1]
+
+            except:
+                dungrak = no_exday.find_all("em", {"class": "X"})[1]
+                buho = ""
+        dungrak = buho + dungrak.find("span", {"class": "blind"}).text
+        dungrak = float(dungrak)
+
+        list.append({"stock_name": stock_name, "sise": sise, "dungrak": dungrak, "start_price": start_price})
+
+        return list
+
+    def Stock_name(self):
+        self.curs.execute('select*from stock_name')
+        re = self.curs.fetchall()
+        name_list =[]
+        for i in range(len(re)):
+            n = re[i]['stock_name']
+            name_list.append(n)
+        return name_list
+
+
+    def Thema_Stock(self,stock):
+        sql ='select thema from stock_thema where stock_name=%s'
+        self.curs.execute(sql,stock)
+        re = self.curs.fetchall()
+        return re
+
+    def Today_Stock(self):
+        # 시간변수
+        today = datetime.datetime.today()
+        yester_day = today - datetime.timedelta(days=1)
+        time = today.strftime("%H:%M:%S")
+        today = today.strftime("%Y%m%d")
+        yester_day = yester_day.strftime("%Y%m%d")
+
+        today_list = []
+        # db에서 t_stock call
+        try:
+            try:
+                call_sql = "select stock_name , count from blog_stock_" + today
+                self.curs.execute(call_sql)
+            except:
+                call_sql = "select stock_name , count from blog_stock_" + yester_day
+                self.curs.execute(call_sql)
+
+            response = self.curs.fetchall()
+            result = []
+            for j in range(len(response)):
+                if int(response[j]["count"]) > 4:
+                    result.append(response[j])
+                else:
+                    pass
+
+            for i in range(len(result)):
+                n = result[i]["stock_name"]
+                try:
+                    data = self.stock_naver_sise(n)
+                    p = data[0]["sise"]
+                    r = float(data[0]["dungrak"])
+                    c = result[i]["count"]
+                    s = data[0]["start_price"]
+                except:
+                    p = "error"
+                    r = 0
+                    c = "error"
+                    s = "error"
+                today_list.append({"stock_name":n, "sise":p, "dungrak":r, "start_price":s,"time":time})
+        except:
+            today_list.append({"stock_name":"None", "sise":"None", "dungrak":0, "start_price":"None","time":time})
+        return today_list
+
+    def korea_index(self):
+        today = datetime.datetime.today()
+        time = today.strftime("%H:%M:%S")
+        list = []
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.90.18 Safari/537.36'
+        headers = {'User-Agent': user_agent}
+        url = "https://finance.naver.com/sise/"
+        source = requests.get(url, headers=headers).text  # requests 모듈을 통해 텍스트로 끌어옴
+        soup = BeautifulSoup(source, 'html.parser')
+        kospi = soup.find("li", {"onmouseover": "moveIndex('tab_sel1')"})
+        kosdaq = soup.find("li", {"onmouseover": "moveIndex('tab_sel2')"})
+
+        try:
+            kospi_index = kospi.find("span", {"id": "KOSPI_now"}).text
+            kospi_dungrak = kospi.find("span", {"class":"num_s"}).text[-9:-4]
+            kosdaq_index = kosdaq.find("span", {"id": "KOSDAQ_now"}).text
+            kosdaq_dungrak = kosdaq.find("span", {"class": "num_s"}).text[-9:-4]
+
+        except:
+            kospi_index = kospi.find("span", {"class": "num num2"}).text
+            kospi_dungrak = kospi.find("span", {"class": "num_s num_s2"}).text[-9:-4]
+            kosdaq_index = kosdaq.find("span", {"class": "num"}).text
+            kosdaq_dungrak = kosdaq.find("span", {"class": "num_s"}).text[-9:-4]
+
+        list.append({"market": "kospi", "indice": kospi_index, "rate": float(kospi_dungrak),"time":time})
+        list.append({"market": "kosdaq", "indice": kosdaq_index, "rate": float(kosdaq_dungrak),"time":time})
+
+        return list
+
+
+    #다우,나스닥 선물 지수
+    def america_index(self):
+        url ="https://kr.investing.com/indices/indices-futures"
+        source = requests.get(url, headers=self.headers).text  # requests 모듈을 통해 텍스트로 끌어옴
+        soup = BeautifulSoup(source, 'html.parser')
+        future = soup.select('#cross_rates_container > table > tbody > tr ')
+        #a= soup.find('#cross_rates_container > table > tbody > tr > td')
+        #print(a)
+
+        future_current=[]
+        for i in future:
+            data =list(i)
+            market =data[3].get_text()
+            indice = data[7].get_text()
+            rate = data[15].get_text()
+            if rate[0] == "+":
+                rate = rate[1:-1]
+            else:
+                rate = rate[:-1]
+            time = data[17].get_text()
+            time = data[17].get_text()
+            future_current.append({'market':market ,"indice":indice,'rate' : float(rate), "time" : time})
+        result = future_current[:3]
+
+        return  result
+
+
+'''
+#Tday 가격함수를 db에 저장한후 콜 클래스
+class stock_future():
+
+    def __init__(self):
+        self.conn = pymysql.connect(host='dydb.c2x6adsfvww3.ap-northeast-2.rds.amazonaws.com', user='qwasdf123',
+                               password='rlarltjq1!', db='stock_db', charset='utf8',
+                               cursorclass=pymysql.cursors.DictCursor)
+        self.curs = self.conn.cursor()
+
+        self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.90.18 Safari/537.36'
+        self.headers = {'User-Agent': self.user_agent}
+
+
+    def stock_code(self,stock_name):
+        stock_code_sql = "select stock_code from stock_name where stock_name='"+stock_name+"'"
+        self.curs.execute(stock_code_sql)
+        result =self.curs.fetchall()[0]["stock_code"]
+        return result
+
+
+    # 네이버에서 실시간  시세크롤링 / 실시간 반영 / 신호속도 느림
 
     def Stock_name(self):
         self.curs.execute('select*from stock_name')
@@ -105,9 +277,9 @@ class stock_future():
         result = future_current[:3]
 
         return  result
-
+'''
 if __name__=="__main__":
     start = datetime.datetime.now()
     stock_future = stock_future()
-    print(stock_future.save_t_tock_date())
+    print(stock_future.Today_Stock())
     #print(stock_future.korea_index())
--- mainix/sise_future/tests.py
+++ mainix/sise_future/tests.py
@@ -1,120 +1,160 @@
-from bs4 import BeautifulSoup
-import urllib.request
-import requests
-import xml.etree.ElementTree as ET
 import pymysql
+from bs4 import BeautifulSoup
+from functools import partial
+import time
+import requests
 import datetime
-conn = pymysql.connect(host='dydb.c2x6adsfvww3.ap-northeast-2.rds.amazonaws.com', user='qwasdf123',
-                               password='rlarltjq1!', db='stock_db', charset='utf8',
-                               cursorclass=pymysql.cursors.DictCursor)
-curs = conn.cursor()
+from selenium import webdriver
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.90.18 Safari/537.36'
-headers = {'User-Agent': user_agent}
+import concurrent.futures
 
-def stock_code( stock_name):
-    stock_code_sql = "select stock_code from stock_name where stock_name='" + stock_name + "'"
-    curs.execute(stock_code_sql)
-    result = curs.fetchall()[0]["stock_code"]
-    return result
+stock_db_conn = pymysql.connect(host='dydb.c2x6adsfvww3.ap-northeast-2.rds.amazonaws.com', user='qwasdf123',
+                                password='rlarltjq1!', db='stock_db', charset='utf8',
+                                cursorclass=pymysql.cursors.DictCursor)
+stock_db_curs = stock_db_conn.cursor()
 
-def stock_naver_sise(stock_name):
+user_agent ='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.90.18 Safari/537.36'
+path = "C:\\Users\\Doyoeng_Desktop\\Desktop\\django_coding\\Stock_BigData\\chromedriver"
+headers = {'User-Agent' : user_agent }
 
-    list = []
-    code = stock_code(stock_name)
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.146 Whale/2.6.90.18 Safari/537.36'
-    headers = {'User-Agent': user_agent}
-    url = "http://finance.naver.com/item/sise.nhn?code=" + code
-    source = requests.get(url, headers=headers).text  # requests 모듈을 통해 텍스트로 끌어옴
+
+def Stock_name():
+    stock_db_curs.execute('select*from stock_name')
+    re = stock_db_curs.fetchall()
+    name_list = []
+    for i in range(len(re)):
+        n = re[i]['stock_name']
+        name_list.append(n)
+    return name_list
+
+def Thema_Stock(stock):
+    sql ='select thema from stock_thema where stock_name=%s'
+    stock_db_curs.execute(sql,stock)
+    re = stock_db_curs.fetchall()
+    return re
+
+
+def Naver_Blog_url_word(days,keyword,page_num):
+    days = days
+    keyword = keyword
+    cafe_today = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
+    cafe_days_ago = (datetime.datetime.today() - datetime.timedelta(days)).strftime("%Y%m%d%H%M%S")
+
+    url_list1 = []
+    blog_text =[]
+
+    url_naver_blog = "https://search.naver.com/search.naver?" \
+                     "date_from=" + cafe_today + \
+                     "&date_option=2" \
+                     "&date_to=" + cafe_days_ago + \
+                     "&dup_remove=1&nso=p%3A1d&post_blogurl=&post_blogurl_without=&query=" + keyword + \
+                     "&sm=tab_pge&srchby=all&st=sim" \
+                     "&where=post&start="+page_num
+
+    source = requests.get(url_naver_blog, headers=headers).text  # requests 모듈을 통해 텍스트로 끌어옴
     soup = BeautifulSoup(source, 'html.parser')
-    no_today = soup.find("p", {"class": "no_today"})
-    no_exday = soup.find("p", {"class": "no_exday"})
-    sise = no_today.find("span", {"class": "blind"}).text
-    price_table = soup.find("table")
-    start_price = price_table.find_all("td", {"class": "first"})[1]
-    start_price = start_price.find("span", {"class": "blind"}).text
-    try:
-        dungrak = no_exday.find_all("em", {"class": "no_up"})[1]
-        buho = dungrak.text[1]
+    for text in soup.select("span.inline > a.url"):
+        crowl = text.get_text()
+        blog_url = "http://m."+crowl
+        url_list1.append(blog_url)
+    url_set = set(url_list1)
+    url_list2 = list(url_set)
 
-    except:
+    for url in url_list2:
         try:
-            dungrak = no_exday.find_all("em", {"class": "no_down"})[1]
-            buho = dungrak.text[1]
-
+            options = webdriver.ChromeOptions()
+            options.add_argument('headless')
+            options.add_argument('window-size=1920x1080')
+            options.add_argument("disable-gpu")
+            driver = webdriver.Chrome(path,chrome_options=options)
+            driver.get(url)
+            cont = driver.find_element_by_xpath('//*[@id="viewTypeSelector"]').text
         except:
-            dungrak = no_exday.find_all("em", {"class": "X"})[1]
-            buho = ""
-    dungrak = buho + dungrak.find("span", {"class": "blind"}).text
-    dungrak = float(dungrak)
+            cont='error'
 
-    list.append({"stock_name": stock_name, "sise": sise, "dungrak": dungrak, "start_price": start_price})
 
-    return list
+        blog_text.append(cont)
 
-def save_t_tock_date():
-    #sql 쿼리문
-    insert_sql = "insert into naver_sise(stock_name, sise, dungrak, start_price) values(%s,%s,%s,%s)"
-    delet_sql = "delete from naver_sise;"
+       #driver.close()
+        time.sleep(2)
+    return blog_text
 
-    #시간변수
+
+def Blog_find_word(keyword, page_num):
+
+    word_list = Naver_Blog_url_word(1, keyword, page_num)
+    sentence_anal = {}
+    stock_list = []
+    stock_name_list = Stock_name()
+    for sentence in word_list:
+        for stock_name in stock_name_list:
+            if sentence.find(stock_name) > 0 and sentence.count(stock_name) > 1:
+                cou = sentence.count(stock_name)
+                sentence_anal[stock_name] = cou
+    sentence_anal = sorted(sentence_anal.items(), key=lambda t: t[1], reverse=True)
+    for i in range(len(sentence_anal)):
+        stock_name = sentence_anal[i][0]
+        cnt = sentence_anal[i][1]
+        try:
+            try:
+                thema = Thema_Stock(stock_name + " ")[0]['thema']
+            except:
+                thema = Thema_Stock(stock_name)[0]['thema']
+        except:
+            thema = "None"
+        stock_list.append({'stock_name': stock_name, "thema": thema, "count": cnt})
+
+    return stock_list
+
+
+def blog_multi_and_dbSave(keyword):
     today = datetime.datetime.today()
-    yester_day = today -datetime.timedelta(days=1)
-    time =today.strftime("%H:%M:%S")
+    ago_day = today - datetime.timedelta(days=2)
     today = today.strftime("%Y%m%d")
-    yester_day = yester_day.strftime("%Y%m%d")
+    ago_day = ago_day.strftime("%Y%m%d")
+    page_num_list = []
+    fun = partial(Blog_find_word, keyword)
+    stock_list = []
+    for i in range(10):
+        if i == 0:
+            page_num = str(1)
+        else:
+            page_num = str(i) + str(1)
+        page_num_list.append(page_num)
 
-    today_list =[]
-    curs.execute(delet_sql)
-    #db에서 t_stock call
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for st in executor.map(fun, page_num_list):
+            stock_list.append(st)
+
+    creat_sql = "create table blog_stock_" + today + "(id INT NOT NULL AUTO_INCREMENT," \
+                                                     "stock_name varchar(50), thema varchar(50),count varchar(10),PRIMARY KEY (id))"
+    delete_sql = "drop table blog_stock_" + ago_day + ";"
+    insert_sql = "insert into blog_stock_" + today + "(stock_name, thema, count) values(%s,%s,%s)"
+
     try:
-        try :
-            call_sql = "select stock_name , count from blog_stock_"+today
-            curs.execute(call_sql)
-        except:
-            call_sql ="select stock_name , count from blog_stock_"+yester_day
-            curs.execute(call_sql)
+        stock_db_curs.execute(creat_sql)
+    except:
+        stock_db_curs.execute("SET SQL_SAFE_UPDATES = 0;")
+        stock_db_curs.execute("delete from blog_stock_" + today)
 
-        response = curs.fetchall()
-        result =[]
-        for j in range(len(response)):
-            if int(response[j]["count"]) > 4 :
-                result.append(response[j])
+    for i in range(len(stock_list)):
+        for j in range(len(stock_list[i])):
+            c = stock_list[i][j]["count"]
+            if int(c) > 4:
+                n = stock_list[i][j]["stock_name"]
+                t = stock_list[i][j]["thema"]
+                stock_db_curs.execute(insert_sql, (n, t, c))
+                stock_db_conn.commit()
             else:
                 pass
-
-        for i in range(len(result)):
-            n = result[i]["stock_name"]
-            try:
-                data = stock_naver_sise(n)
-                p = data[0]["sise"]
-                r = data[0]["dungrak"]
-                c = result[i]["count"]
-                s = data[0]["start_price"]
-            except:
-                p ="error"
-                r ="error"
-                c ="error"
-                s ="error"
-            curs.execute(insert_sql, (n, p, r, s))
-
-
-
+    try:
+        stock_db_curs.execute(delete_sql)
     except:
-        curs.execute(insert_sql, ("None", "None","None", "None"))
-    conn.commit()
-    curs.close()
-    return
-
-
+        pass
+    stock_db_conn.close()
+    return keyword + " Blog Bigdata Analysis for Stock Save Complete"
 
 if __name__=="__main__":
-    print(save_t_tock_date())
-
-'''
-url = "http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108"
-request = urllib.request.urlopen(url)
-xml = request.read()
-soup = BeautifulSoup(xml, "html.parser")
-print(soup.find("location"))
-'''
+    kst = datetime.timezone(datetime.timedelta(hours=0))
+    today = datetime.datetime.now(kst)
+    print(today)
